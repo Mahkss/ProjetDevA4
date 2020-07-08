6b8e004367da971a8e67a96f02341cb52fb1b810
Mahkss <max.pavet@viacesi.fr>
2020-07-08 14:32:44 +0200

    Remplacer les zip

---
+++ App/Client/Groupe6_CSharpCLient/ProjetDevA4/Service/AuthenticationService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetDevA4.Service
+{
+    class AuthenticationService
+    {
+        public static string Login(string login, string password, string Apptoken)
+        {
+            var client = new ServiceReference1.AuthentificationClient();
+
+            var UserToken = client.CheckLogin(login, password, Apptoken);
+
+            client.Close();
+
+            return UserToken;
+            //return 10;
+        }
+    }
+}
