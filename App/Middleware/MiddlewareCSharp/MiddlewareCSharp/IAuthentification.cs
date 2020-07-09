884b750d0afce131c63e1a71cee82831f9abcd48
Mahkss <max.pavet@viacesi.fr>
2020-07-09 14:52:06 +0200

    Add files via upload

---
+++ App/Middleware/MiddlewareCSharp/MiddlewareCSharp/IAuthentification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MiddlewareCSharp
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom d'interface "IAuthentification" à la fois dans le code et le fichier de configuration.
+    [ServiceContract]
+    public interface IAuthentification
+    {
+        [OperationContract]
+        void DoWork();
+
+        [OperationContract]
+        string CheckLogin(string login, string pwd, string appToken);
+    }
+}
---
+++ App/Middleware/MiddlewareCSharp/MiddlewareCSharp/IUncipher.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MiddlewareCSharp
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom d'interface "IUncipher" à la fois dans le code et le fichier de configuration.
+    [ServiceContract]
+    public interface IUncipher
+    {
+        [OperationContract]
+        void DoWork();
+
+        [OperationContract]
+        Message GetUncryptedInfo(Message msg);
+
+    }
+
+    [DataContract]
+    public class Message
+    {
+        private Boolean statusOp = true;
+        private String[] data;
+        private String operationName;
+        private String tokenApp;
+        private String tokenUSer;
+
+        [DataMember]
+        public string TokenApp { get => tokenApp; set => tokenApp = value; }
+        [DataMember]
+        public string TokenUSer { get => tokenUSer; set => tokenUSer = value; }
+        [DataMember]
+        public string OperationName { get => operationName; set => operationName = value; }
+        [DataMember]
+        public string[] Data { get => data; set => data = value; }
+        [DataMember]
+        public bool StatusOp { get => statusOp; set => statusOp = value; }
+    }
+
+
+    
+}
