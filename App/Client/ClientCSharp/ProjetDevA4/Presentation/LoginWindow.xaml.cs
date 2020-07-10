d4c33d328f1efc23d118021cd7dc6aaec51ad3c7
Mahkss <max.pavet@viacesi.fr>
2020-07-10 16:57:43 +0200

    Add files via upload

---
+++ App/Client/ClientCSharp/ProjetDevA4/Presentation/LoginWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Linq;
+using System.Security.Authentication;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ProjetDevA4.Logic;
+
+namespace ProjetDevA4.Presentation
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+            this.Show();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            Authentication.Login(this);
+        }
+    }
+}
