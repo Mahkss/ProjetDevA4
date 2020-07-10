fcac118552dc744f34f20a36eb864d6c151429c5
Mahkss <max.pavet@viacesi.fr>
2020-07-10 03:10:08 +0200

    Parallélisation Client + Upload DOssier
    Parallélisation des taches clientes améliorée + Envoi de Dossier au service possible

---
+++ App/Client/Groupe6_CSharpCLient/ProjetDevA4/ProjetDevA4/Logic/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using ProjetDevA4.Presentation;
+using ProjetDevA4.Service;
+
+namespace ProjetDevA4.Logic
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+		private void App_StartUp(object sender, StartupEventArgs e)
+		{
+            _ = new LoginWindow();
+        }
+
+        }
+
+}
